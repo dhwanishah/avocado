6f446d13f81d2c4fdf2487f14654a5920142d7d5
Dhwani Shah <dshah202@gmail.com>
2015-10-13 02:50:40 -0500

    Created static public directory and defined static path in app.js

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ var app = express();
 /* Routes require */
 var root_route = require('./routes/index');
 
+/* Views setup */
 app.set("views", './views');
 app.set("view engine", 'jade');
 
+
+/* Main routing paths setup */
 app.use("/", root_route);
+
+
+/* Static files path setup */
+app.use(express.static('./public'));
 
 app.listen(3000);
 
