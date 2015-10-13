93618b9b0ef65456999e75eb75cae7468961e968
Dhwani Shah <dshah202@gmail.com>
2015-10-13 02:34:31 -0500

    added views and views engine dependency in app.js. Fixed the routes

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 /* Routes require */
 var root_route = require('./routes/index');
+
+app.set("views", './views');
+app.set("view engine", 'jade');
 
 app.use("/", root_route);
 
